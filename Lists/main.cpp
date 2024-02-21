d765daacbabd07334613b9e64ac5ef4e6f729cc1
BogdAN1337228 <kolganov_25@mail.ru>
2024-02-21 14:15:35 +0700

    -Add function Contains -Add some comments -Add an interface for entering values

--- Lists/main.cpp
+++ Lists/main.cpp
@@ -1,10 +1,7 @@
-﻿#include<iostream>
-#include<stdio.h>
-/*
-Реализовать функции для обработки списка(по варианту) : ​​​
--
--Определить, входит ли список L1 в L2. Вернуть адрес начала вхождения (нулевой адрес в противном случае).
-*/
+﻿#include<stdio.h>
+#include<iostream>
+#define _CRT_SECURE_NO_WARNINGS
+
 struct Item
 {
 	int value;
@@ -13,6 +10,7 @@ struct Item
 
 int ListSize(Item* item);
 
+//Добавляет объект в конец списка
 void Push_back(int value, Item** item) {
 	if (*item == NULL) {
 		*item = (Item*)malloc(sizeof(Item));
@@ -25,6 +23,7 @@ void Push_back(int value, Item** item) {
 	}
 }
 
+//Возращает указатель на объект в заданной позиции
 Item* GetItemByPos(int pos, Item* item) {
 	//Неверный ввод позиции
 	if (pos <= 0) {
@@ -45,6 +44,7 @@ Item* GetItemByPos(int pos, Item* item) {
 	return item;
 }
 
+//Возращает значение объекта в заданной позиции(минимальное значение при неверных данных)
 int GetValueByPos(int pos, Item* item) {
 	if (pos <= 0) {
 		printf("Enter position > 0\n");
@@ -64,6 +64,7 @@ int GetValueByPos(int pos, Item* item) {
 	return item->value;
 }
 
+//Добавляет в список объект в позицию pos и возвращает отредактированный список
 Item* Push(int value, int pos, Item** item) {
 	//Неверный ввод позиции
 	if (pos <= 0) {
@@ -78,8 +79,8 @@ Item* Push(int value, int pos, Item** item) {
 		return tmp;
 	}
 	//Неверный ввод позиции
-	else if (pos > ListSize(*item)) {
-		printf("Enter position > list size\n");
+	else if (pos > ListSize(*item)+1) {
+		printf("Enter position < list size\n");
 		return *item;
 	}
 	//Создаем два указателя, в которых хранятся объект, на место которого мы хотим поставить наш объект и предыдущий от него объект
@@ -96,6 +97,7 @@ Item* Push(int value, int pos, Item** item) {
 	return *item;
 }
 
+//Удаляет из список объект с позицией pos и возвращает отредактированный список
 Item* Pop(int pos, Item** item) {
 	//Неверный ввод позиции
 	if (pos <= 0) {
@@ -110,7 +112,7 @@ Item* Pop(int pos, Item** item) {
 	}
 	//Неверный ввод позиции
 	else if (pos > ListSize(*item)) {
-		printf("Enter position > list size\n");
+		printf("Enter position < list size\n");
 		return *item;
 	}
 	//Создаем два указателя, в которых хранятся предыдущий и следующий объект от выбранного
@@ -125,6 +127,7 @@ Item* Pop(int pos, Item** item) {
 	return *item;
 }
 
+//Возвращает кол-во элементов в списке
 int ListSize(Item* item) {
 	int cnt = 0;
 	while (item != NULL) {
@@ -134,7 +137,12 @@ int ListSize(Item* item) {
 	return cnt;
 }
 
+//Выводит все значения в списке
 void PrintList(Item* item) {
+	if (item == NULL) {
+		printf("List is empty!\n");
+		return;
+	}
 	printf("List of items:\n");
 	while (item != NULL) {
 		printf("%d ", item->value);
@@ -143,9 +151,9 @@ void PrintList(Item* item) {
 	printf("\n");
 }
 
-//Найти среднее арифметическое значение элементов списка
+//Находит и возвращает среднее арифметическое значение элементов списка
 float ArithmeticMean(Item* item) {
-	int result = 0, size = ListSize(item);
+	float result = 0, size = ListSize(item);
 
 	while (item != NULL) {
 		result += item->value;
@@ -155,26 +163,83 @@ float ArithmeticMean(Item* item) {
 	return result / size;
 }
 
+//Определяет, входит ли список item1 в item2. Возвращает адрес начала вхождения (нулевой адрес в противном случае)
+Item* Contains(Item* item1, Item* item2) {
+	bool isContains = true;
+	if (item1 == NULL || item2 == NULL) {
+		return NULL;
+	}
+	//Создаем указатели для промежуточного хранения адреса, с которого должны начинаться следующие итерации главного цикла
+	Item* head1 = item1;
+	Item* head2 = item2;
+	//проходим по item2, чтобы найти точку входа с одинаковыми значениями
+	while (item2 != NULL) {
+		if (item2->value == item1->value) {
+			//item1 возвращаем в самое начало, а для head2 задаем адрес, в котором мы были до входа во вложенный цикл, чтобы потом вернуться к этому значению
+			item1 = head1;
+			head2 = item2;
+			while (item1 != NULL) {
+				//Если условие ни разу не выполняется, значит item1 входит в item2, иначе ищем следующую точку входа
+				if (item1->value != item2->value) {
+					isContains = false;
+					break;
+				}
+				item1 = item1->next;
+				item2 = item2->next;
+			}
+			item2 = head2;
+			//Если условие ни разу не выполнелось, поэтому возвращаем полученное значение
+			if (isContains) {
+				return item2;
+			}
+		}
+		item2 = item2->next;
+	}
+	//item1 не содержит item2
+	return NULL;
+}
+
 
 int main() {
-	Item* arr = (Item*)malloc(sizeof(Item));
-	arr->value = 0;
-	arr->next = NULL;
+	Item* arr = NULL;
+	int length, val, pos;
+	printf("Enter count of values -> ");
+	scanf_s("%d", &length);
+	
+	for (int i = 0; i < length; i++)
+	{
+		printf("%d) ", i+1);
+		scanf_s("%d", &val);
+		Push_back(val, &arr);
+	}
+	
+	printf("Enter value -> ");
+	scanf_s("%d", &val);
+	printf("Enter position in list -> ");
+	scanf_s("%d", &pos);
+	arr = Push(val, pos, &arr);
 
-	Push_back(1, &arr);
-	Push_back(2, &arr);
-	Push_back(4, &arr);
-
-	arr = Push(3, 4, &arr);
 	PrintList(arr);
-	printf("%d\n", ListSize(arr));
+	printf("The List size: %d\n", ListSize(arr));
 
-	arr = Pop(3,&arr);
+	/*arr = Pop(3,&arr);
 	PrintList(arr);
-	printf("%d\n", ListSize(arr));
+	printf("%d\n", ListSize(arr));*/
 
 	printf("The Arithmetic mean: %f\n", ArithmeticMean(arr));
 
-	free(arr);
+	Item* arr1 = NULL;
+	printf("Enter count of values -> ");
+	scanf_s("%d", &length);
+
+	for (int i = 0; i < length; i++)
+	{
+		printf("%d) ", i+1);
+		scanf_s("%d", &val);
+		Push_back(val, &arr1);
+	}
+  	Item* L1 = Contains(arr1,arr);
+
+	PrintList(L1);
 	return 0;
 }
